dc4eee126b4457d65e019cdf8c71b2e6af7bf95e
Andrea Di Persio <git@andreadipersio.com>
2018-01-16 00:44:45 +0100

    On Makefile.php detect OS to use correct shell.

--- Makefile.php
+++ Makefile.php
@@ -1,4 +1,12 @@
-SHELL := `which bash`
+# vim: noexpandtab filetype=make
+
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+	SHELL := /usr/bin/bash
+endif
+ifeq ($(UNAME_S),Darwin)
+	SHELL := /usr/local/bin/bash
+endif
 
 BIN ?= ${HOME}/bin
 COMPOSER = php ${BIN}/composer.phar
@@ -11,5 +19,8 @@ ${BIN}/composer.phar:
 
 laravel: ${BIN}/composer.phar
 	${COMPOSER} global require 'laravel/laravel'
+
+test:
+	composer help
 
 .PHONY: laravel
