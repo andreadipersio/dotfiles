1b5b42301166d0a6587da2d5a53ba620de884ff7
Andrea Di Persio <git@andreadipersio.com>
2019-01-06 13:43:32 +0100

    Uncomment package initialize

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,18 @@
-;(package-initialize)
+(package-initialize)
 
 (load "~/.emacs.d/my.el")
 (load "~/.emacs.d/my-packages.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck-rust flycheck ripgrep rg racer projectile magit ivy company cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
