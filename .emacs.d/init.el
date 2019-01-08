dc2345eea830b3af8a36b777583cd1352c1adf41
Andrea Di Persio <andrea.dipersio@shopify.com>
2019-01-08 18:44:58 +0100

    Add Emacs format-sql command
    Format sql in the current buffer using sqlformat
    (part of python sqlparse library)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 
 (load "~/.emacs.d/my.el")
 (load "~/.emacs.d/my-packages.el")
+(load "~/.emacs.d/my-local-packages.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
